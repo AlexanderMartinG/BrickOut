3c5bf9aa65837df97b4d52a0967a7d196c4b93dc
AlexanderMartinG <32973849+AlexanderMartinG@users.noreply.github.com>
2017-10-23 23:14:25 -0500

    Add files via upload

---
+++ Scripts/Raqueta.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raqueta : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AgrandarRaqueta(){
+		//cambia la escala de x a 10
+		transform.localScale = new Vector3 (10,transform.localScale.y, transform.localScale.z);
+
+	}
+
+	void AchicarRaqueta(){
+		//cambia la escala de x a 6
+		transform.localScale = new Vector3 (6,transform.localScale.y, transform.localScale.z);
+
+	}
+
+	//Inicia la corrutina
+	void IniciarCorrutinaAgrandar(){
+
+		StartCoroutine (CorrutinaAgrandar());
+
+	}
+
+	//corrutina agrandar
+	IEnumerator CorrutinaAgrandar(){
+		AgrandarRaqueta ();
+		yield return new WaitForSeconds (10f); //esperar X segundos
+		AchicarRaqueta();
+	}
+
+}
